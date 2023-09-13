baa9ab63de872571f8943ef640a9c52ef6f65d35
felixkappa <118398978+felixkappa@users.noreply.github.com>
2023-09-12 18:56:36 -0600

    Add files via upload

---
+++ EVA7_STACK/src/eva7_stack/EVA7_STACK.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva7_stack;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA7_STACK {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Inicia Main");
+        A();
+        System.out.println("Fin Main");
+        
+        
+        // TODO code application logic here
+    }
+    
+    public static void A(){
+    System.out.println("Inicia A");
+    B();
+    System.out.println("Fin A");
+
+    }
+    public static void B(){
+    System.out.println("Inicia B");
+    System.out.println("Fin B");
+
+    }
+    
+}
